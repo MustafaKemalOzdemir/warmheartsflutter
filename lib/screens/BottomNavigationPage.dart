d0a863c4b709a0acb17476ca113ad80ab7b8c50e
Mustafa Kemal Özdemir <MustafaKemalOzdemir@users.noreply.github.com>
2020-07-17 22:40:45 +0300

    nearbyMap initial added

--- lib/screens/BottomNavigationPage.dart
+++ lib/screens/BottomNavigationPage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:warm_hearts_flutter/screens/AddPostPage.dart';
 import 'package:warm_hearts_flutter/screens/HomePage.dart';
+import 'package:warm_hearts_flutter/screens/NearbyMapPage.dart';
 import 'package:warm_hearts_flutter/screens/ProfilePage.dart';
 
 class BottomNavigationPage extends StatefulWidget {
@@ -15,12 +16,13 @@ class _BottomNavigationPageState extends State<BottomNavigationPage> {
     return Scaffold(
       body: _getBody(),
       bottomNavigationBar: BottomNavigationBar(
-        elevation: 2,
-        showSelectedLabels: true,
+        unselectedItemColor: Colors.grey,
+        selectedItemColor: Colors.blueAccent,
         currentIndex: _pageMode,
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Home')),
-          BottomNavigationBarItem(icon: Icon(Icons.add), title: Text('Profile')),
+          BottomNavigationBarItem(icon: Icon(Icons.location_on), title: Text('Nearby')),
+          BottomNavigationBarItem(icon: Icon(Icons.add), title: Text('Add Post')),
           BottomNavigationBarItem(icon: Icon(Icons.account_circle), title: Text('Profile')),
         ],
         onTap: (state){
@@ -35,8 +37,9 @@ class _BottomNavigationPageState extends State<BottomNavigationPage> {
   Widget _getBody(){
     switch(_pageMode){
       case 0: return HomePage();
-      case 1: return AddPostPage();
-      case 2: return ProfilePage();
+      case 1: return NearbyMapPage();
+      case 2: return AddPostPage();
+      case 3: return ProfilePage();
       default: return Center(child: Text('Error on Navigation'));
     }
   }
